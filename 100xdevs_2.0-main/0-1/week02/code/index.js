0d71ab16e393fc7601d085f22211870ca08f8e51
Ishaan Jain <ishaanj2612@gmail.com>
2025-08-25 00:13:06 +0530

    Added few changes

--- 100xdevs_2.0-main/0-1/week02/code/index.js
+++ 100xdevs_2.0-main/0-1/week02/code/index.js
@@ -1,11 +1,18 @@
 // Express Basic - creating a HTTP server
 // to install - npm install express
-
+// to run- node index.js - open localhost:3000
 
 const express = require("express");
-
 const app = express();
+const port = 3000;
 
-app.listen(3000);
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+app.post("/backend-api/conversation", (req, res) => {
+    res.send("Hit the Endpoint");
+});
 
-// to run- node index.js - open localhost:3000
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ 100xdevs_2.0-main/0-1/week02/ishaan.js
@@ -0,0 +1,18 @@
+function onDone() {
+  console.log("Operation completed with data:");
+}
+
+setTimeout(onDone, 10);
+
+for (let i = 0; i < 10; i++) {
+  console.log("Iteration for A:", i);
+}
+for (let i = 0; i < 10; i++) {
+  console.log("Iteration for B:", i);
+}
+for (let i = 0; i < 10; i++) {
+  console.log("Iteration for C:", i);
+}
+for (let i = 0; i < 10; i++) {
+  console.log("Iteration for D:", i);
+}
